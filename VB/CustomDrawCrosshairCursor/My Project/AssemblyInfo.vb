a9523445833af61ce17f06efdf17b09372b8fb8f
DevExpressExampleBot <fakeEmail>
2019-12-11 14:15:20 +0300

    Source auto update [skip ci]

---
+++ VB/CustomDrawCrosshairCursor/My Project/AssemblyInfo.vb
@@ -0,0 +1,59 @@
+﻿' Developer Express Code Central Example:
+' How to custom draw a crosshair cursor
+' 
+' This example shows how to provide custom appearance of the crosshair cursor
+' using the ChartControl.CustomDrawCrosshair event. This event is invoked when you
+' click the Custom Draw Crosshair Cursor button on the form. If you wish to
+' display crosshair axis lines and labels on a chart before custom drawing the
+' crosshair cursor, set the CrosshairOptions.ShowArgumentLine
+' (http://help.devexpress.com/#XtraCharts/DevExpressXtraChartsCrosshairOptions_ShowArgumentLinetopic),
+' CrosshairOptions.ShowArgumentLabels
+' (http://help.devexpress.com/#XtraCharts/DevExpressXtraChartsCrosshairOptions_ShowArgumentLabelstopic),
+' CrosshairOptions.ShowValueLabels
+' (http://help.devexpress.com/#XtraCharts/DevExpressXtraChartsCrosshairOptions_ShowValueLabelstopic)
+' and CrosshairOptions.ShowValueLine
+' (http://help.devexpress.com/#XtraCharts/DevExpressXtraChartsCrosshairOptions_ShowValueLinetopic)
+' properties to true. Note that the customization of the crosshair cursor is
+' provided for the CrosshairOptions.SnapMode
+' (http://documentation.devexpress.com/#XtraCharts/DevExpressXtraChartsCrosshairOptions_SnapModetopic)
+' property set to NearestArgument.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E4307
+
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("CustomDrawCrosshairCursor")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CustomDrawCrosshairCursor")>
+<Assembly: AssemblyCopyright("Copyright ©  2013")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f106e0ba-43be-4484-aba6-1d78de2199ca")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' [assembly: AssemblyVersion("1.0.*")]
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
