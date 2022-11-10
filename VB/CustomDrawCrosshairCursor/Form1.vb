ee9e6c2c04bcb6b0b4b5235947162c5e754f0fe8
DevExpressExampleBot <fakeEmail>
2022-11-10 11:30:41 +0400

    Source auto update [skip ci]

--- VB/CustomDrawCrosshairCursor/Form1.vb
+++ VB/CustomDrawCrosshairCursor/Form1.vb
@@ -1,62 +1,54 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Drawing
 Imports System.Windows.Forms
 Imports DevExpress.XtraCharts
 
 Namespace CustomDrawCrosshairCursor
-	Partial Public Class Form1
-		Inherits Form
-		Private handleCustomDraw As Boolean
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			handleCustomDraw = True
-			AddHandler chartControl1.CustomDrawCrosshair, AddressOf chartControl1_CustomDrawCrosshair
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub chartControl1_CustomDrawCrosshair(ByVal sender As Object, ByVal e As CustomDrawCrosshairEventArgs) Handles chartControl1.CustomDrawCrosshair
-			If (Not handleCustomDraw) Then
-				Return
-			End If
+        Private handleCustomDraw As Boolean
 
-			' Specify the crosshair argument line color, dash style and thickness.
-			e.CrosshairLineElement.Color = Color.Green
-			e.CrosshairLineElement.LineStyle.DashStyle = DashStyle.DashDot
-			e.CrosshairLineElement.LineStyle.Thickness = 3
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			' Specify  the back color for crosshair argument label. 
-			For Each axisLabelElement As CrosshairAxisLabelElement In e.CrosshairAxisLabelElements
-				axisLabelElement.BackColor = Color.Blue
-			Next axisLabelElement
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            handleCustomDraw = True
+            AddHandler chartControl1.CustomDrawCrosshair, AddressOf chartControl1_CustomDrawCrosshair
+        End Sub
 
-			For Each element As CrosshairElement In e.CrosshairElements
+        Private Sub chartControl1_CustomDrawCrosshair(ByVal sender As Object, ByVal e As CustomDrawCrosshairEventArgs)
+            If Not handleCustomDraw Then Return
+            ' Specify the crosshair argument line color, dash style and thickness.
+            e.CrosshairLineElement.Color = Color.Green
+            e.CrosshairLineElement.LineStyle.DashStyle = DashStyle.DashDot
+            e.CrosshairLineElement.LineStyle.Thickness = 3
+            ' Specify  the back color for crosshair argument label. 
+            For Each axisLabelElement As CrosshairAxisLabelElement In e.CrosshairAxisLabelElements
+                axisLabelElement.BackColor = Color.Blue
+            Next
 
-				' Specify the color, dash style and thickness for the crosshair value lines. 
-				element.LineElement.Color = Color.DarkViolet
-				element.LineElement.LineStyle.DashStyle = DashStyle.Dash
-				element.LineElement.LineStyle.Thickness = 2
+            For Each element As CrosshairElement In e.CrosshairElements
+                ' Specify the color, dash style and thickness for the crosshair value lines. 
+                element.LineElement.Color = Color.DarkViolet
+                element.LineElement.LineStyle.DashStyle = DashStyle.Dash
+                element.LineElement.LineStyle.Thickness = 2
+                ' Specify the  text color and back color for the crosshair value labels.
+                element.AxisLabelElement.TextColor = Color.Red
+                element.AxisLabelElement.BackColor = Color.Yellow
+                ' Specify the text color and marker size for the crosshair  cursor label that shows series. 
+                element.LabelElement.TextColor = Color.Red
+                element.LabelElement.MarkerSize = New Size(15, 15)
+            Next
 
-				' Specify the  text color and back color for the crosshair value labels.
-				element.AxisLabelElement.TextColor = Color.Red
-				element.AxisLabelElement.BackColor = Color.Yellow
-
-
-				' Specify the text color and marker size for the crosshair  cursor label that shows series. 
-				element.LabelElement.TextColor = Color.Red
-				element.LabelElement.MarkerSize = New Size(15, 15)
-
-			Next element
-
-			For Each groupHeaderElement As CrosshairGroupHeaderElement In e.CrosshairGroupHeaderElements
-
-				' Specify the text, text color and font for the crosshair group header element. 
-				groupHeaderElement.Text = "Custom draw"
-				groupHeaderElement.TextColor = Color.Green
-				groupHeaderElement.Font = New Font(Me.Font, FontStyle.Bold)
-			Next groupHeaderElement
-		End Sub
-	End Class
+            For Each groupHeaderElement As CrosshairGroupHeaderElement In e.CrosshairGroupHeaderElements
+                ' Specify the text, text color and font for the crosshair group header element. 
+                groupHeaderElement.Text = "Custom draw"
+                groupHeaderElement.TextColor = Color.Green
+                groupHeaderElement.Font = New Font(Font, FontStyle.Bold)
+            Next
+        End Sub
+    End Class
 End Namespace
