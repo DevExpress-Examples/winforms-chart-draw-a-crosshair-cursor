5ff53fe19b5c5998769eaf5ee802069ca9f4b6e2
DevExpressExampleBot <fakeEmail>
2022-11-10 11:32:26 +0400

    Source auto update [skip ci]

--- VB/CustomDrawCrosshairCursor/Form1.Designer.vb
+++ VB/CustomDrawCrosshairCursor/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace CustomDrawCrosshairCursor
-    Partial Public Class Form1
+Namespace CustomDrawCrosshairCursor
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,47 +12,47 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
-            Dim xyDiagram1 As New DevExpress.XtraCharts.XYDiagram()
-            Dim series1 As New DevExpress.XtraCharts.Series()
-            Dim pointSeriesLabel1 As New DevExpress.XtraCharts.PointSeriesLabel()
-            Dim seriesPoint1 As New DevExpress.XtraCharts.SeriesPoint("1", New Object() { (DirectCast(50R, Object))})
-            Dim seriesPoint2 As New DevExpress.XtraCharts.SeriesPoint("2", New Object() { (DirectCast(34R, Object))})
-            Dim seriesPoint3 As New DevExpress.XtraCharts.SeriesPoint("3", New Object() { (DirectCast(55R, Object))})
-            Dim seriesPoint4 As New DevExpress.XtraCharts.SeriesPoint("4", New Object() { (DirectCast(77R, Object))})
-            Dim splineSeriesView1 As New DevExpress.XtraCharts.SplineSeriesView()
-            Dim series2 As New DevExpress.XtraCharts.Series()
-            Dim pointSeriesLabel2 As New DevExpress.XtraCharts.PointSeriesLabel()
-            Dim seriesPoint5 As New DevExpress.XtraCharts.SeriesPoint("1", New Object() { (DirectCast(22R, Object))})
-            Dim seriesPoint6 As New DevExpress.XtraCharts.SeriesPoint("2", New Object() { (DirectCast(11R, Object))})
-            Dim seriesPoint7 As New DevExpress.XtraCharts.SeriesPoint("3", New Object() { (DirectCast(9R, Object))})
-            Dim seriesPoint8 As New DevExpress.XtraCharts.SeriesPoint("4", New Object() { (DirectCast(8R, Object))})
-            Dim splineSeriesView2 As New DevExpress.XtraCharts.SplineSeriesView()
-            Dim sideBySideBarSeriesLabel1 As New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+            Dim xyDiagram1 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram()
+            Dim series1 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+            Dim pointSeriesLabel1 As DevExpress.XtraCharts.PointSeriesLabel = New DevExpress.XtraCharts.PointSeriesLabel()
+            Dim seriesPoint1 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("1", New Object() {(CObj((50R)))})
+            Dim seriesPoint2 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("2", New Object() {(CObj((34R)))})
+            Dim seriesPoint3 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("3", New Object() {(CObj((55R)))})
+            Dim seriesPoint4 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("4", New Object() {(CObj((77R)))})
+            Dim splineSeriesView1 As DevExpress.XtraCharts.SplineSeriesView = New DevExpress.XtraCharts.SplineSeriesView()
+            Dim series2 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+            Dim pointSeriesLabel2 As DevExpress.XtraCharts.PointSeriesLabel = New DevExpress.XtraCharts.PointSeriesLabel()
+            Dim seriesPoint5 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("1", New Object() {(CObj((22R)))})
+            Dim seriesPoint6 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("2", New Object() {(CObj((11R)))})
+            Dim seriesPoint7 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("3", New Object() {(CObj((9R)))})
+            Dim seriesPoint8 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("4", New Object() {(CObj((8R)))})
+            Dim splineSeriesView2 As DevExpress.XtraCharts.SplineSeriesView = New DevExpress.XtraCharts.SplineSeriesView()
+            Dim sideBySideBarSeriesLabel1 As DevExpress.XtraCharts.SideBySideBarSeriesLabel = New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
             Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
             Me.button1 = New System.Windows.Forms.Button()
             Me.panel1 = New System.Windows.Forms.Panel()
-            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(xyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(series1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(pointSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(splineSeriesView1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(series2, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(pointSeriesLabel2, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(splineSeriesView2, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(sideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((xyDiagram1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((series1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((pointSeriesLabel1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((splineSeriesView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((series2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((pointSeriesLabel2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((splineSeriesView2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((sideBySideBarSeriesLabel1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.panel1.SuspendLayout()
             Me.SuspendLayout()
             ' 
@@ -71,21 +73,22 @@
             Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.chartControl1.Location = New System.Drawing.Point(0, 0)
             Me.chartControl1.Name = "chartControl1"
-            pointSeriesLabel1.LineVisibility = DevExpress.Utils.DefaultBoolean.True
+            pointSeriesLabel1.LineVisibility = DevExpress.Utils.DefaultBoolean.[True]
             series1.Label = pointSeriesLabel1
             series1.Name = "Series 1"
-            series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() { seriesPoint1, seriesPoint2, seriesPoint3, seriesPoint4})
+            series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() {seriesPoint1, seriesPoint2, seriesPoint3, seriesPoint4})
             series1.View = splineSeriesView1
-            pointSeriesLabel2.LineVisibility = DevExpress.Utils.DefaultBoolean.True
+            pointSeriesLabel2.LineVisibility = DevExpress.Utils.DefaultBoolean.[True]
             series2.Label = pointSeriesLabel2
             series2.Name = "Series 2"
-            series2.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() { seriesPoint5, seriesPoint6, seriesPoint7, seriesPoint8})
+            series2.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() {seriesPoint5, seriesPoint6, seriesPoint7, seriesPoint8})
             series2.View = splineSeriesView2
-            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() { series1, series2}
-            sideBySideBarSeriesLabel1.LineVisibility = DevExpress.Utils.DefaultBoolean.True
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() {series1, series2}
+            sideBySideBarSeriesLabel1.LineVisibility = DevExpress.Utils.DefaultBoolean.[True]
             Me.chartControl1.SeriesTemplate.Label = sideBySideBarSeriesLabel1
             Me.chartControl1.Size = New System.Drawing.Size(480, 316)
             Me.chartControl1.TabIndex = 0
+            AddHandler Me.chartControl1.CustomDrawCrosshair, New DevExpress.XtraCharts.CustomDrawCrosshairEventHandler(AddressOf Me.chartControl1_CustomDrawCrosshair)
             ' 
             ' button1
             ' 
@@ -96,6 +99,7 @@
             Me.button1.TabIndex = 1
             Me.button1.Text = "Custom Draw Crosshair Cursor"
             Me.button1.UseVisualStyleBackColor = True
+            AddHandler Me.button1.Click, New System.EventHandler(AddressOf Me.button1_Click)
             ' 
             ' panel1
             ' 
@@ -115,25 +119,24 @@
             Me.Controls.Add(Me.panel1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(xyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(pointSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(splineSeriesView1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(series1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(pointSeriesLabel2, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(splineSeriesView2, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(series2, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(sideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((xyDiagram1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((pointSeriesLabel1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((splineSeriesView1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((series1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((pointSeriesLabel2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((splineSeriesView2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((series2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((sideBySideBarSeriesLabel1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.panel1.ResumeLayout(False)
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
+#End Region
+        Private chartControl1 As DevExpress.XtraCharts.ChartControl
 
-        Private WithEvents chartControl1 As DevExpress.XtraCharts.ChartControl
-        Private WithEvents button1 As System.Windows.Forms.Button
+        Private button1 As System.Windows.Forms.Button
+
         Private panel1 As System.Windows.Forms.Panel
     End Class
 End Namespace
-
