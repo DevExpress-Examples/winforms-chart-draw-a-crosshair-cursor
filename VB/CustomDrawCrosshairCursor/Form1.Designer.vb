045a83f869ef8ae808cf89144db51573ed780644
DevExpressExampleBot <fakeEmail>
2022-11-29 20:09:22 +0400

    Source auto update [skip ci]

---
+++ VB/CustomDrawCrosshairCursor/Form1.Designer.vb
@@ -0,0 +1,137 @@
+Namespace CustomDrawCrosshairCursor
+
+    Partial Class Form1
+
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
+
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim xyDiagram1 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram()
+            Dim series1 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+            Dim pointSeriesLabel1 As DevExpress.XtraCharts.PointSeriesLabel = New DevExpress.XtraCharts.PointSeriesLabel()
+            Dim seriesPoint1 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint(1R, New Object() {(CObj((50R)))})
+            Dim seriesPoint2 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint(2R, New Object() {(CObj((34R)))})
+            Dim seriesPoint3 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint(3R, New Object() {(CObj((55R)))})
+            Dim seriesPoint4 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint(4R, New Object() {(CObj((77R)))})
+            Dim splineSeriesView1 As DevExpress.XtraCharts.SplineSeriesView = New DevExpress.XtraCharts.SplineSeriesView()
+            Dim series2 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+            Dim pointSeriesLabel2 As DevExpress.XtraCharts.PointSeriesLabel = New DevExpress.XtraCharts.PointSeriesLabel()
+            Dim seriesPoint5 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint(1R, New Object() {(CObj((22R)))})
+            Dim seriesPoint6 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint(2R, New Object() {(CObj((11R)))})
+            Dim seriesPoint7 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint(3R, New Object() {(CObj((9R)))})
+            Dim seriesPoint8 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint(4R, New Object() {(CObj((8R)))})
+            Dim splineSeriesView2 As DevExpress.XtraCharts.SplineSeriesView = New DevExpress.XtraCharts.SplineSeriesView()
+            Dim sideBySideBarSeriesLabel1 As DevExpress.XtraCharts.SideBySideBarSeriesLabel = New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+            Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+            Me.panel1 = New System.Windows.Forms.Panel()
+            Me.checkEdit1 = New DevExpress.XtraEditors.CheckEdit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((xyDiagram1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((series1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((pointSeriesLabel1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((splineSeriesView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((series2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((pointSeriesLabel2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((splineSeriesView2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((sideBySideBarSeriesLabel1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.panel1.SuspendLayout()
+            CType((Me.checkEdit1.Properties), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' chartControl1
+            ' 
+            Me.chartControl1.CrosshairOptions.ShowArgumentLabels = True
+            Me.chartControl1.CrosshairOptions.ShowValueLabels = True
+            Me.chartControl1.CrosshairOptions.ShowValueLine = True
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
+            Me.chartControl1.Diagram = xyDiagram1
+            Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.chartControl1.Legend.Name = "Default Legend"
+            Me.chartControl1.Location = New System.Drawing.Point(0, 0)
+            Me.chartControl1.Name = "chartControl1"
+            pointSeriesLabel1.LineVisibility = DevExpress.Utils.DefaultBoolean.[True]
+            series1.Label = pointSeriesLabel1
+            series1.Name = "Series 1"
+            series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() {seriesPoint1, seriesPoint2, seriesPoint3, seriesPoint4})
+            series1.View = splineSeriesView1
+            pointSeriesLabel2.LineVisibility = DevExpress.Utils.DefaultBoolean.[True]
+            series2.Label = pointSeriesLabel2
+            series2.Name = "Series 2"
+            series2.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() {seriesPoint5, seriesPoint6, seriesPoint7, seriesPoint8})
+            series2.View = splineSeriesView2
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() {series1, series2}
+            sideBySideBarSeriesLabel1.LineVisibility = DevExpress.Utils.DefaultBoolean.[True]
+            Me.chartControl1.SeriesTemplate.Label = sideBySideBarSeriesLabel1
+            Me.chartControl1.Size = New System.Drawing.Size(480, 316)
+            Me.chartControl1.TabIndex = 0
+            ' 
+            ' panel1
+            ' 
+            Me.panel1.Controls.Add(Me.checkEdit1)
+            Me.panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+            Me.panel1.Location = New System.Drawing.Point(0, 316)
+            Me.panel1.Name = "panel1"
+            Me.panel1.Size = New System.Drawing.Size(480, 63)
+            Me.panel1.TabIndex = 0
+            ' 
+            ' checkEdit1
+            ' 
+            Me.checkEdit1.Dock = System.Windows.Forms.DockStyle.Right
+            Me.checkEdit1.Location = New System.Drawing.Point(309, 0)
+            Me.checkEdit1.Name = "checkEdit1"
+            Me.checkEdit1.Properties.Caption = "Custom Draw Crosshair Cursor"
+            Me.checkEdit1.Size = New System.Drawing.Size(171, 63)
+            Me.checkEdit1.TabIndex = 0
+            AddHandler Me.checkEdit1.CheckedChanged, New System.EventHandler(AddressOf Me.OnCheckEditCheckedChanged)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(480, 379)
+            Me.Controls.Add(Me.chartControl1)
+            Me.Controls.Add(Me.panel1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType((xyDiagram1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((pointSeriesLabel1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((splineSeriesView1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((series1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((pointSeriesLabel2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((splineSeriesView2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((series2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((sideBySideBarSeriesLabel1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.panel1.ResumeLayout(False)
+            CType((Me.checkEdit1.Properties), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
+
+#End Region
+        Private chartControl1 As DevExpress.XtraCharts.ChartControl
+
+        Private panel1 As System.Windows.Forms.Panel
+
+        Private checkEdit1 As DevExpress.XtraEditors.CheckEdit
+    End Class
+End Namespace
---
+++ VB/CustomDrawCrosshairCursor/Form1.vb
@@ -0,0 +1,56 @@
+Imports System
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports DevExpress.Drawing
+Imports DevExpress.XtraCharts
+
+Namespace CustomDrawCrosshairCursor
+
+    Public Partial Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub OnCheckEditCheckedChanged(ByVal sender As Object, ByVal e As EventArgs)
+            If checkEdit1.Checked Then
+                AddHandler chartControl1.CustomDrawCrosshair, AddressOf OnChartControlCustomDrawCrosshair
+            Else
+                RemoveHandler chartControl1.CustomDrawCrosshair, AddressOf OnChartControlCustomDrawCrosshair
+            End If
+        End Sub
+
+        Private Sub OnChartControlCustomDrawCrosshair(ByVal sender As Object, ByVal e As CustomDrawCrosshairEventArgs)
+            ' Specify the crosshair argument line color, dash style and thickness.
+            e.CrosshairLineElement.Color = Color.Green
+            e.CrosshairLineElement.LineStyle.DashStyle = DashStyle.DashDot
+            e.CrosshairLineElement.LineStyle.Thickness = 3
+            ' Specify the back color for the crosshair argument axis label. 
+            For Each axisLabelElement As CrosshairAxisLabelElement In e.CrosshairAxisLabelElements
+                axisLabelElement.BackColor = Color.Blue
+            Next
+
+            For Each group As CrosshairElementGroup In e.CrosshairElementGroups
+                Dim groupHeaderElement As CrosshairGroupHeaderElement = group.HeaderElement
+                ' Specify the text, text color and font for the crosshair group header element. 
+                groupHeaderElement.Text = "Custom draw"
+                groupHeaderElement.TextColor = Color.Green
+                groupHeaderElement.DXFont = New DXFont("SegoeUI", 12, DXFontStyle.Bold)
+                ' Obtain the first series.
+                Dim element As CrosshairElement = group.CrosshairElements(0)
+                ' Specify the color, dash style and thickness for the crosshair value lines. 
+                element.LineElement.Color = Color.DarkViolet
+                element.LineElement.LineStyle.DashStyle = DashStyle.Dash
+                element.LineElement.LineStyle.Thickness = 2
+                ' Specify the text color and back color for the crosshair value labels.
+                element.AxisLabelElement.TextColor = Color.Red
+                element.AxisLabelElement.BackColor = Color.Yellow
+                ' Format the text shown for the series in the crosshair cursor label. Specify the text color and marker size. 
+                element.LabelElement.TextColor = Color.Red
+                element.LabelElement.MarkerSize = New Size(15, 15)
+                element.LabelElement.Text = String.Format("{0}: A={1}; V={2}", element.Series.Name, element.SeriesPoint.Argument, element.SeriesPoint.Values(0))
+            Next
+        End Sub
+    End Class
+End Namespace
